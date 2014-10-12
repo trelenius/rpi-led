a726829868246b2fbe6ed7f675ff6700e51d824e
Tralalalmak <rutakusd@gmail.com>
2014-10-12 17:05:18 +0200

    fixed VCS

--- rpi-led.py
+++ rpi-led.py
@@ -1,14 +1,8 @@
 __author__ = 'Simon'
-
 import sys
 sys.path.append('pycharm-debug-py3k.egg')
-import pydevd
-pydevd.settrace('192.168.13.100', port=51200, stdoutToServer=True, stderrToServer=True)
 
-print "Hello, World!"
-a = 50
-b = 12
-c = a+b
-d = c + 1+a
-print "Debug"
-pydevd.stoptrace()
+import pydevd
+debugServerIP = '192.168.13.100'
+debugServerPort = 51200
+pydevd.settrace(debugServerIP, port=debugServerPort, stdoutToServer=True, stderrToServer=True)
